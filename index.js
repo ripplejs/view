892e4f8bffe67b17f3ba3908e70e6929e5654a02
Anthony Short <antshort@gmail.com>
2014-02-08 15:48:24 -0800

    A working version of the view for the compiler. With tests

--- index.js
+++ index.js
@@ -1,73 +1,197 @@
+var domify = require('domify');
 var emitter = require('emitter');
-var Observer = require('observer');
+var model = require('model');
+var interpolate = require('interpolate');
 
-/**
- * Compiled template view
- * @param {Element} el    Parent element
- * @param {Scope} scope   The scope includes all models for this view
- */
-function View(el, scope) {
-  this.el = el;
-  this.children = []; // Child Views
-  this.bindings = []; // Attribute bindings
-  this.scope = scope; // Current scope
-}
+module.exports = function(template, process) {
 
-/**
- * Mixin emitter
- */
-emitter(View.prototype);
+  /**
+   * Stores the state of the view.
+   *
+   * @type {Function}
+   */
+  var State = model();
 
-/**
- * Get the value of a property on the view
- * @param  {String} key
- * @return {Mixed}
- */
-View.prototype.get = function(key) {
-  this.scope.get(key);
+  /**
+   * Stores the properties of the view.
+   *
+   * @type {Function}
+   */
+  var Properties = model();
+
+  /**
+   * The view controller
+   *
+   * @param {Object} state
+   */
+  function View(state) {
+    this.el = domify(template);
+    this.state = new State(state);
+    this.props = new Properties();
+    this.filters = {};
+    this.View = View;
+    View.emit('construct', this);
+    if(process) process(this);
+  }
+
+  /**
+   * Use a plugin
+   *
+   * @return {View}
+   */
+  View.use = function(fn){
+    fn(this);
+    return this;
+  };
+
+  /**
+   * Add an interpolation filter
+   *
+   * @param {String} name
+   * @param {Function} fn
+   *
+   * @return {View}
+   */
+  View.filter = function(name, fn) {
+    View.on('construct', function(view){
+      view.filters[name] = fn;
+    });
+    return this;
+  };
+
+  /**
+   * Add a computed state property
+   *
+   * @return {void}
+   */
+  View.computed = function(key, deps, fn) {
+    State.computed(key, deps, fn);
+    return this;
+  };
+
+  /**
+   * Stores a reference to all of the properties
+   * this view can have
+   *
+   * @type {Array}
+   */
+  View.props = [];
+
+  /**
+   * Create a new property on the view
+   *
+   * @return {void}
+   */
+  View.prop = function(name){
+    View.on('construct', function(view){
+      Object.defineProperty(view, name, {
+        get: function(){
+          return view.props.get(name);
+        },
+        set: function(val){
+          view.props.set(name, val);
+        }
+      });
+    });
+    View.props.push(name);
+    return this;
+  };
+
+  /**
+   * Mixin emitter
+   */
+  emitter(View);
+  emitter(View.prototype);
+
+  /**
+   * Get the value of a property on the view
+   * @param  {String} key
+   * @return {Mixed}
+   */
+  View.prototype.get = function(key) {
+    return this.state.get(key);
+  };
+
+  /**
+   * Set the value of a property on the view
+   * @param  {String} key
+   * @param  {Mixed}  value
+   * @return {void}
+   */
+  View.prototype.set = function(key, value) {
+    this.state.set(key, value);
+  };
+
+  /**
+   * Run an interpolation on the string using the state. Whenever
+   * the model changes it will render the string again
+   *
+   * @param {String} str
+   * @param {Function} callback
+   *
+   * @return {Function} a function to unbind the interpolation
+   */
+  View.prototype.interpolate = function(str, callback) {
+    var state = this.state;
+    var filters = this.filters;
+    var attrs = interpolate.props(str);
+    if(attrs.length === 0) return;
+    function render() {
+      return interpolate(str, state.get(attrs), filters);
+    }
+    callback(render());
+    return this.state.change(attrs, function(){
+      callback(render());
+    });
+  };
+
+  /**
+   * Append this view to an element
+   *
+   * @param {Element} el
+   *
+   * @return {View}
+   */
+  View.prototype.mount = function(el) {
+    el.appendChild(this.el);
+    this.emit('mount', el);
+    return this;
+  };
+
+  /**
+   * Remove the element from the DOM
+   *
+   * @return {View}
+   */
+  View.prototype.unmount = function() {
+    if(!this.el.parentNode) return;
+    this.el.parentNode.removeChild(this.el);
+    this.emit('unmount');
+    return this;
+  };
+
+  /**
+   * Causes all bindings that are watching
+   * this view to attach themselves
+   *
+   * @return {View}
+   */
+  View.prototype.bind = function(){
+    this.emit('bind', this);
+    View.emit('bind', this);
+    return this;
+  };
+
+  /**
+   * Unbind all events and binding from the view,
+   * leaving just the element in place
+   * @return {View}
+   */
+  View.prototype.unbind = function(){
+    this.emit('unbind', this);
+    View.emit('unbind', this);
+    return this;
+  };
+
+  return View;
 };
-
-/**
- * Unbind all events and binding from the view,
- * leaving just the element in place
- * @return {View}
- */
-View.prototype.unbind = function(){
-  this.children.forEach(function(child){
-    child.unbind();
-  });
-  this.bindings.forEach(function(binding){
-    binding.unbind();
-  });
-  return this;
-};
-
-/**
- * Remove the view element from the DOM
- * @return {View}
- */
-View.prototype.remove = function(){
-  if(!this.el.parentNode) return this;
-  this.el.parentNode.removeChild(this.el);
-  return this;
-};
-
-/**
- * Remove this view from the DOM and unbind everything
- * @return {View}
- */
-View.prototype.destroy = function(){
-  return this.unbind().remove();
-};
-
-/**
- * Append this view to an element
- * @param  {Element} el
- * @return {View}
- */
-View.prototype.appendTo = function(el) {
-  el.appendChild(this.el);
-  return this;
-};
-
-module.exports = View;
---
+++ test/tests.js
@@ -0,0 +1,157 @@
+var assert = require('assert');
+var createView = require('view');
+
+describe('View', function(){
+  var View;
+
+  beforeEach(function(){
+    View = createView('<div></div>');
+  });
+
+  it('should construct', function(){
+    var view = new View();
+  })
+
+  it('should construct with state properties', function(){
+    var view = new View({
+      foo: 'bar'
+    });
+    assert(view.get('foo') === 'bar');
+  })
+
+  it('should set state', function(){
+    var view = new View();
+    view.set('foo', 'bar');
+    assert(view.get('foo') === 'bar');
+  })
+
+  it('should interpolate a string', function(done){
+    var view = new View();
+    view.set('foo', 'bar');
+    view.interpolate('{{foo}}', function(val){
+      assert(val === 'bar');
+      done();
+    });
+  })
+
+  it('should add interpolation filters', function(done){
+    View.filter('noobify', function(){
+      return 'noob';
+    });
+    var view = new View();
+    view.set('foo', 'bar');
+    view.interpolate('{{foo | noobify}}', function(val){
+      assert(val === 'noob');
+      done();
+    });
+  })
+
+  it('should have chainable static methods', function(){
+    View
+      .use(function(){})
+      .filter('noobify', function(){
+        return 'noob';
+      })
+      .prop('foo')
+      .computed('bar', ['baz'], function(){});
+
+    var view = new View();
+  })
+
+  it('should watch a string for changes', function(done){
+    var count = 0;
+    var view = new View();
+    view.set('foo', 'bar');
+    view.interpolate('{{foo}}', function(val){
+      count++;
+      if(count === 1) assert(val === 'bar');
+      if(count === 2) {
+        assert(val === 'baz');
+        done();
+      }
+    });
+    view.set('foo', 'baz');
+  })
+
+  it('should mount to an element and fire an event', function(done){
+    var view = new View();
+    view.on('mount', function(){
+      assert(document.body.contains(view.el));
+      done();
+    })
+    view.mount(document.body);
+  })
+
+  it('should unmount and fire an event', function(done){
+    var view = new View();
+    view.mount(document.body);
+    view.on('unmount', function(){
+      done();
+    })
+    view.unmount();
+  })
+
+  it('should bind and fire an event', function(done){
+    var view = new View();
+    view.on('bind', function(){
+      done();
+    })
+    view.bind();
+  })
+
+  it('should unbind and fire an event', function(done){
+    var view = new View();
+    view.on('unbind', function(){
+      done();
+    })
+    view.unbind();
+  })
+
+  it('should get properies', function(){
+    View.prop('foo');
+    var view = new View();
+    view.props.set('foo', 'bar');
+    assert(view.foo === 'bar');
+  })
+
+  it('should set properties', function(){
+    View.prop('foo');
+    var view = new View();
+    view.foo = 'bar';
+    assert(view.props.get('foo') === 'bar');
+  })
+
+  it('should have computed properties', function(){
+    View.computed('fullname', ['firstname', 'lastname'], function(first, last){
+      return [first,last].join(' ');
+    });
+    var view = new View({
+      'firstname': 'Bruce',
+      'lastname': 'Willis'
+    });
+    assert(view.get('fullname') === 'Bruce Willis');
+    view.set('firstname', 'Danny');
+    assert(view.get('fullname') === 'Danny Willis');
+  })
+
+  it('should have computed properties with nested dependencies', function(){
+    View.computed('fullname', ['names.first', 'names.last'], function(first, last){
+      return [first,last].join(' ');
+    });
+    var view = new View({
+      names: {
+        first: 'Bruce',
+        last: 'Willis'
+      }
+    });
+    assert(view.get('fullname') === 'Bruce Willis');
+    view.set({
+      names: {
+        first: 'Jet',
+        last: 'Li'
+      }
+    });
+    assert(view.get('fullname') === 'Jet Li');
+  })
+
+})
